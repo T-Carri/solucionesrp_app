6fa9c81066af71735095736e13cd75c9c7ff92ad
T-Carri <bryan_narutito@hotmail.com>
2023-05-19 15:12:19 -0600

    1952023-311

--- src/contexts/auth-context.js
+++ src/contexts/auth-context.js
@@ -1,4 +1,5 @@
-import { createContext, useContext, useEffect, useState } from 'react'
+import { createContext, useContext, useEffect, useReducer, useRef, useState  } from 'react';
+
 import {
   onAuthStateChanged,
   createUserWithEmailAndPassword,
@@ -6,19 +7,127 @@ import {
   signOut,
 } from 'firebase/auth'
 import { auth } from '../config/firebase'
+
+import PropTypes from 'prop-types';
+
+
+const HANDLERS = {
+    INITIALIZE: 'INITIALIZE',
+    SIGN_IN: 'SIGN_IN',
+    SIGN_OUT: 'SIGN_OUT'
+  };
+  
+
+  const initialState = {
+    isAuthenticated: false,
+    isLoading: true,
+    user: null
+  };
+  
+
+
+
+
+  const handlers = {
+    [HANDLERS.INITIALIZE]: (state, action) => {
+      const user = action.payload;
+  
+      return {
+        ...state,
+        ...(
+          // if payload (user) is provided, then is authenticated
+          user
+            ? ({
+              isAuthenticated: true,
+              isLoading: false,
+              user
+            })
+            : ({
+              isLoading: false
+            })
+        )
+      };
+    },
+    [HANDLERS.SIGN_IN]: (state, action) => {
+      const user = action.payload;
+  
+      return {
+        ...state,
+        isAuthenticated: true,
+        user
+      };
+    },
+    [HANDLERS.SIGN_OUT]: (state) => {
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null
+      };
+    }
+  };
+
+
+
+
+  const reducer = (state, action) => (
+    handlers[action.type] ? handlers[action.type](state, action) : state
+  );
+  
+
+
 const AuthContext = createContext();
 export default AuthContext
+
 export const UserAuth = () => {
   return useContext(AuthContext);
 };
+
+
+
 export const AuthContextProvider = ({ children }) => {
+//ojo en ejemplo usa un prop y luego destructura a children...
+//parece ser que yo lo hago directo. En caso de error verificar
+//verificar eso.
+
+    const initialized = useRef(false);
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
   // const dispatch=useDispatch() ;
+
+  //function to put at useEffect with reducer func
+   
+const initialize = async()=>{
+
+    if (initialized.current){
+        return;
+    }
+
+    initialized.current= true
+
+    
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
   //const firestore= getFirestore(app)
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
         if (user) {
+
           setUser({
             uid: user.uid,
             email: user.email,
@@ -32,6 +141,9 @@ export const AuthContextProvider = ({ children }) => {
       })
     return () => unsubscribe();
   },[]);
+
+
+
   //version normal
   const signup  = (email, password) => {
     return  createUserWithEmailAndPassword(auth, email, password)
